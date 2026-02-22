02c0270bb1e0506cc67590d5602804b28bd1b7e1
Sina Yekta <ceenayekta@gmail.com>
2026-02-23 01:22:59 +0330

    feat: use mock datas in layouts

--- eboom-frontend/app/(dashboard)/page.tsx
+++ eboom-frontend/app/(dashboard)/page.tsx
@@ -1,17 +1,18 @@
 "use client";
 
+import { ChartAreaInteractive } from "@/src/components/details/chart-area-interactive";
+import { DataTable } from "@/src/components/details/data-table";
+import { SectionCards } from "@/src/components/details/section-cards";
 import { useRouter } from "next/navigation";
+import data from "@/src/_mocks/data.json"
 const DashboardPage = () => {
-    const router = useRouter();
-  return <div>
-    <button onClick={() => {
-        localStorage.removeItem("accessToken");
-        localStorage.removeItem("refreshToken");
-        router.push("/login");
-    }}>
-        Logout
-    </button>   
-  </div>;
+  return <>
+    <div className="px-4 lg:px-6">
+      <ChartAreaInteractive />
+    </div>
+    <SectionCards />
+    <DataTable data={data} /> 
+  </>;
 };
 
 export default DashboardPage;
--- eboom-frontend/src/components/LayoutProvider.tsx
+++ eboom-frontend/src/components/LayoutProvider.tsx
@@ -2,7 +2,10 @@ import { AppSidebar } from "@/src/components/layout/app-sidebar"
 import {
   SidebarInset,
   SidebarProvider,
-} from "@/components/ui/sidebar"
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 export default function LayoutProvider({ children }: { children: React.ReactNode }) {
   return (
@@ -16,9 +19,31 @@ export default function LayoutProvider({ children }: { children: React.ReactNode
     >
       <AppSidebar variant="inset" />
       <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mr-2 data-[orientation=vertical]:h-4"
+            />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbLink href="#">
+                    Canvas
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Income</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 h-full">
+            <div className="flex flex-col gap-4 md:gap-6 h-full">
               {children}
             </div>
           </div>
