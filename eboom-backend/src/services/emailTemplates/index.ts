8b8a89ab4d72c2580e5255f7b1d25b4edd053403
karen.misaghian2005@gmail.com <karen.misaghian2005@gmail.com>
2026-07-14 01:18:57 +0330

    fix: resolve some minor issues

---
+++ eboom-backend/src/services/emailTemplates/index.ts
@@ -0,0 +1,16 @@
+export {
+  renderVerificationEmail,
+  renderVerificationEmailText,
+} from "./verification";
+export {
+  renderPasswordResetEmail,
+  renderPasswordResetEmailText,
+} from "./passwordReset";
+export {
+  buildBudgetAlertsEmailSubject,
+  buildOverdueEmailSubject,
+  renderBudgetAlertsEmail,
+  renderBudgetAlertsEmailText,
+  renderOverdueNotificationsEmail,
+  renderOverdueNotificationsEmailText,
+} from "./notifications";
